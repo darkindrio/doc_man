a78ba2475f5b1f9d4b9a84a701c1c712bde2d432
jparevalo <jparevalo@miuandes.cl>
2017-05-15 16:59:54 -0400

    Implemented devise for users

--- doc_man/config/routes.rb
+++ doc_man/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
+  authenticate :user do
+      resources :users
+      root to: 'documents#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
