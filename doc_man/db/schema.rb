4b84e885129cc2150ec2fc646efa1f25d445f147
ababbott <ababbott@miuandes.cl>
2017-05-15 20:58:18 -0400

    Add Document sacffold

--- doc_man/db/schema.rb
+++ doc_man/db/schema.rb
@@ -9,16 +9,23 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20170516004247) do
+
+ActiveRecord::Schema.define(version: 20170516005720) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-end
 
-ActiveRecord::Schema.define(version: 20170515204123) do
+  create_table "documents", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_documents_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
