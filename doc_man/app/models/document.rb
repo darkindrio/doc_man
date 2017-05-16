7b1bbc8b9712fdd27978a71944834d7307dd46f6
ababbott <ababbott@miuandes.cl>
2017-05-15 21:16:38 -0400

    Add validation to models

--- doc_man/app/models/document.rb
+++ doc_man/app/models/document.rb
@@ -1,3 +1,13 @@
 class Document < ApplicationRecord
   belongs_to :category
+  validates :title, {
+  	length: { minimum: 3,  maximum: 50 }, 
+  	presence: true,
+  	uniqueness: true
+  	}
+  validates :text, {
+  	length: { minimum: 10 },
+  	presence: true
+  }
+	
 end
--- doc_man/app/models/user.rb
+++ doc_man/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+	validates :first_name,{
+		length: { minimum: 3,  maximum: 30 }, 
+		presence: true
+		} 
+  	validates :last_name,{
+  		length: { minimum: 3,  maximum: 30 }, 
+  		presence: true
+  		} 
+  	validates :email,{
+  		length: { maximum: 50 } , 
+  		presence: true,
+  		uniqueness: true
+  		}  
 end
