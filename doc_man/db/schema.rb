776afb218eb69a22eb8ed70353086eed0017b703
ababbott <ababbott@miuandes.cl>
2017-05-29 22:04:51 -0400

    add user to document table

--- doc_man/db/schema.rb
+++ doc_man/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530013041) do
+ActiveRecord::Schema.define(version: 20170530020036) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170530013041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "is_public"
+    t.         "user"
   end
 
   create_table "users", force: :cascade do |t|
